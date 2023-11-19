91a002dc0c7407a407bb2da8614596226e0d4916
Alberto_Malva <medicalquestio@gmail.com>
2023-11-19 10:04:58 +0100

    Inizializzazione e struttura progetto_4

--- Backend/routes/routes.js
+++ Backend/routes/routes.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const Article = require('../models/article'); // Assicurati che il percorso sia corretto
 
-// Qui definirai le rotte
-
-module.exports = router;
 router.get('/articles', async (req, res) => {
-    try {
-      const articles = await Article.find();
-      res.json(articles);
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-  });
-  
+  try {
+    const articles = await Article.find();
+    res.json(articles);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Qualsiasi altra route va qui
+
+// Alla fine del file
+module.exports = router;
